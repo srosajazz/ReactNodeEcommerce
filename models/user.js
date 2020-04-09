d331eca9894ef9e4e9be25c674e18b2f6ed296fc
Sergio Rosa <scarlosjazz@gmail.com>
2020-04-09 18:16:56 -0400

    Virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -37,3 +37,31 @@ const userSchema = new mongoose.Schema({
         default: []
     }
 }, { timestamps: true });
+
+//virtual field
+userSchema.virtual('password')
+    .set(function (password) {
+        this._password = password,
+            this.salt = uuidv1();
+        this.hashed_password = this.ecryptPassword(password);
+    })
+
+    .get(function () {
+        return this._password
+    });
+
+userSchema.method = {
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+
+        } catch (error) {
+            return '';
+        }
+    }
+}
+
+module.exports = mongoose.model("User", userSchema);
