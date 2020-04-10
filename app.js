36ca8e3789b7adbbdae29f029fecf0416e193344
Sergio Rosa <scarlosjazz@gmail.com>
2020-04-10 10:15:49 -0400

    Using Postman to signup user

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 //import routes
-const useRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 
 //app
 const app = express();
@@ -15,9 +18,14 @@ mongoose.connect(process.env.DATABASE, {
     useUnifiedTopology: true
 }).then(() => console.log('DB Connected'));
 
+// .middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 //routes middleware
-app.use("/api", useRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
