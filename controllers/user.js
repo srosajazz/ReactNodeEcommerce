cd52c0275e8655ba5bfb829283dc5942884223ab
Sergio Rosa <scarlosjazz@gmail.com>
2020-04-10 10:38:01 -0400

    Friendly error message

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
 
@@ -8,9 +9,11 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                err,
+                err: errorHandler(err)
             });
         }
+        user.salt = undefined;
+        user.hashed_password = undefined;
         res.json({
             user,
         })
